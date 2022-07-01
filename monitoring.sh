67664c6daeba22be7a64035c88c84a9ad90ff8f8
sphhh <zzvtrx@gmail.com>
2022-06-30 21:37:42 -0300

    monitoring

--- monitoring.sh
+++ monitoring.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
 total=$(free | grep Mem: | cut -d' ' -f11)
 used=$(free | grep Mem: | cut -d' ' -f18)
-opr=$(($used / $total))
+opr=$(echo "scale=4;"$(($used / $total * 100))) 
 
 echo "#Architecture: $(uname -a)"
 echo "#CPU physical : $(nproc)"
 echo "#vCPU : $(cat /proc/cpuinfo | grep processor | wc -l)"
-echo "#Disk Usage: $(free -h | grep Mem: | cut -d' ' -f20 | cut -d'M' -f1)/$(free -h | grep Mem: | cut -d' ' -f12 | cut -d'i' -f1)B ($opr2%)"
+echo "#Memory Usage: $(free -h | grep Mem: | cut -d' ' -f20 | cut -d'M' -f1)/$(free -h | grep Mem: | cut -d' ' -f12 | cut -d'i' -f1)B ($opr%)"
+echo "#Disk Usage: $nval%"
 echo "#CPU load: $nval%"
-echo "#Last boot: $nval"
-echo "#LVM use: $nval"
-echo "#Connections TCP : $nval"
-echo "#User log: $nval"
-echo "#Network: $nval"
-echo "#Sudo : $nval"
+echo "#Last boot: $(who -d | cut -d' ' -f18) $(who -d | cut -d' ' -f19)"
+zero=0
+if [ $(lvscan | grep ACTIVE | wc -l) -gt $zero ]
+then
+	echo "#LVM use: yes"
+else
+	echo "#LVM use: no"
+fi
+echo "#Connections TCP : $(ss -s | grep TCP: | cut -d' ' -f4) ESTABLISHED"
+echo "#User log: $(who | wc -l)"
+echo "#Network: IP $(ip addr | grep global | cut -d' ' -f6 | cut -d'/' -f1) ($(ip addr | grep ether | cut -d' ' -f6))"
+echo "#Sudo : $(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l) cmd"
 
