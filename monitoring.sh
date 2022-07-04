5af4cbf698c6abdbfc383a4cf564e6b9de2e367a
sphh <72941725+sphhh@users.noreply.github.com>
2022-07-04 19:36:58 -0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -2,28 +2,24 @@
 
 total=$(free | grep Mem: | awk '{print $2}')
 used=$(free | grep Mem: | awk '{print $3}')
-opr=$(echo | awk -v a=$used -v b=$total 'BEGIN {printf "%.2f" , a/b*100}')
+opr=$(awk -v a=$used -v b=$total 'BEGIN {printf "%.2f", a/b*100}')
+loud=$(mpstat | grep all | awk '{print $13}')
+zero=0
 
 echo "#Architecture: $(uname -a)"
 echo "#CPU physical : $(lscpu | grep Socket | wc -l)"
 echo "#vCPU : $(cat /proc/cpuinfo | grep processor | wc -l)"
-
 echo "#Memory Usage: $(free -m | grep Mem: | awk '{print $3}')/$(free -m | grep Mem: | awk '{print $2}')MB ($opr%)"
-
 echo "#Disk Usage: $(df -m --total | grep total | awk '{printf $3}')/$(df -h --total | grep total | awk '{printf $2}')b ($(df -h --total | grep total | awk '{print $5}'))"
-
-echo "#CPU load: $()%"
-
+echo "#CPU load: $(awk -v l=$loud 'BEGIN {printf "%.1f", 100-l}')%"
 echo "#Last boot: $(who -b | awk '{print $3,$4}')"
-zero=0
 if [ $(lvscan | grep ACTIVE | wc -l) -gt $zero ]
 then
-	echo "#LVM use: yes"
+        echo "#LVM use: yes"
 else
-	echo "#LVM use: no"
+        echo "#LVM use: no"
 fi
 echo "#Connections TCP : $(ss -t | grep ESTAB | wc -l) ESTABLISHED"
 echo "#User log: $(who | wc -l)"
 echo "#Network: IP $(hostname -I) ($(ip addr | grep ether | cut -d' ' -f6))"
 echo "#Sudo : $(journalctl _COMM=sudo | grep COMMAND | wc -l) cmd"
-
